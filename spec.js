94b6c3f2912c582653e8a3182223f5d795fb1b51
goncharr <alegoncharuk@gmail.com>
2016-11-15 01:41:48 +0200

    The first tests. Create and Delete

--- spec.js
+++ spec.js
@@ -1,7 +1,29 @@
 describe('suite', function () {
+let URL = 'http://maxcabrera.com/code/todo-list/';
+let input = $('input.enter-todo');
+let newtask = $('todo-list');
+let alltasks = element.all(by.css('.todo-container [type="checkbox"]'));
+let general = element(by.binding('Started'));
 
-    it('test', function () {
-        browser.get('http://maxcabrera.com/code/todo-list/')
-        expect(browser.getTitle()).toBe('Angular2 Seed')
+    it('create task', function () {
+        browser.get(URL);
+        expect(browser.getTitle()).toBe('Angular2 Seed');
+        input.click();
+        input.sendKeys('r',protractor.Key.ENTER);
+        expect(newtask.getText()).toContain('R');
+     })
+
+     it('delete one task', function () {
+        browser.get(URL);
+        expect(browser.getTitle()).toBe('Angular2 Seed');
+        expect(general.getText()).toBe('Master Angular 2');
+        /*expect(last.getText()).not.toContain('build a robot');*/
+    })
+
+    it('delete task', function () {
+        browser.get(URL);
+        expect(browser.getTitle()).toBe('Angular2 Seed');
+        alltasks.click();
+        expect(alltasks.count()).toBe(0);
     })
 })
