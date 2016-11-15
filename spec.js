2f326cbaee7eb0a13b978a72a9fdca94f4626579
Oleksandr Honcharuk <oleksandr.honcharuk@TX.local>
2016-11-15 13:46:40 +0200

    update delete test

--- spec.js
+++ spec.js
@@ -1,29 +1,31 @@
+'use strict'
+
 describe('suite', function () {
 let URL = 'http://maxcabrera.com/code/todo-list/';
 let input = $('input.enter-todo');
 let newtask = $('todo-list');
-let alltasks = element.all(by.css('.todo-container [type="checkbox"]'));
-let general = element(by.binding('Started'));
+let allcheckboxes = element.all(by.css('.todo-container [type="checkbox"]'));
+let list = element.all(by.className('Started'));
 
-    it('create task', function () {
+     it('create task', function () {
         browser.get(URL);
         expect(browser.getTitle()).toBe('Angular2 Seed');
         input.click();
         input.sendKeys('r',protractor.Key.ENTER);
         expect(newtask.getText()).toContain('R');
-     })
+    })
 
      it('delete one task', function () {
         browser.get(URL);
         expect(browser.getTitle()).toBe('Angular2 Seed');
-        expect(general.getText()).toBe('Master Angular 2');
-        /*expect(last.getText()).not.toContain('build a robot');*/
+        allcheckboxes.get(0).click();
+        expect(list.get(0).getText()).not.toBe('Read Parenting Books :)');
     })
 
-    it('delete task', function () {
+     it('delete task', function () {
         browser.get(URL);
         expect(browser.getTitle()).toBe('Angular2 Seed');
-        alltasks.click();
-        expect(alltasks.count()).toBe(0);
+        allcheckboxes.click();
+        expect(allcheckboxes.count()).toBe(0);
     })
 })
